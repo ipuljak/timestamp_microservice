4428e54a7b7ff8aa872f87562c547ca5bea7f17e
Ivan <ivanpulj@gmail.com>
2017-01-25 03:43:39 -0500

    First timestamp commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Server requirements
+const express        = require('express'),
+      bodyparser     = require('body-parser'),
+		  methodOverride = require('method-override'),
+      cors           = require('cors');
+		  app            = express();
+
+// Routes requirements
+const timestamp = require('./routes/timestamp');
+
+// Server setup
+app.use(cors());
+app.use(bodyparser.json({type: '*/*'}));
+app.use(methodOverride('_method'));
+
+// Server index file
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Server routes
+app.use('/', timestamp);
+
+// Server port listen
+app.listen(5000, process.env.IP, () => {
+  console.log('Timestamp API server started on port 5000.');
+});
